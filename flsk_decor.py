ae95ebf82d0cb0441bd8fec79eb2adb377574f75
IvanTusanin <ivantusanin@gmail.com>
2025-02-04 20:46:19 +0300

    ADD

---
+++ flsk_decor.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def films():
+   return render_template("index.html", caption="Фильмы про Гарри")
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/shablon")
+def films2():
+    return render_template("index.html", caption="Гарри Поттер")
+
+@app.route("/person/")
+def person():
+    return render_template("main.html")
+
+if __name__ == "__main__":
+    app.run()
